e911952fa15aa0ed7ee400c81a5be7bd4099c2e1
RuslanLukoshkin <90716808+RuslanLukoshkin@users.noreply.github.com>
2022-05-21 07:00:34 +0300

    Update form.php

--- 6/form.php
+++ 6/form.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8" />
     <link rel="stylesheet" href="./style.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
-    <title>5</title>
+    <title>6</title>
 </head>
 
 <body>
